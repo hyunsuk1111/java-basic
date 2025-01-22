28201233d854b2e7879a7c31a2c88fb4f51659ca
hyunsuk1111 <epsxkf@gmail.com>
2025-01-22 18:39:03 +0900

    오버라이딩 다형성

---
+++ src/extends1/poly/overrriding/Parent.java
@@ -0,0 +1,10 @@
+package extends1.poly.overrriding;
+
+public class Parent {
+
+    public String value = "parent";
+
+    public void method() {
+        System.out.println("Parent.method");
+    }
+}
