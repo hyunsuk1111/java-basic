46c4b5ba1e68a06c1823e8b481f74fa9b84d3906
hyunsuk1111 <epsxkf@gmail.com>
2025-01-31 10:10:05 +0900

    다중구현

---
+++ src/extends1/poly/diamond/DiamondMain.java
@@ -0,0 +1,16 @@
+package extends1.poly.diamond;
+
+public class DiamondMain {
+
+    public static void main(String[] args) {
+        InterfaceA a = new Child();
+
+        a.methodA();
+        a.methodCommon();
+
+        InterfaceB b = new Child();
+
+        b.methodB();
+        b.methodCommon();
+    }
+}
