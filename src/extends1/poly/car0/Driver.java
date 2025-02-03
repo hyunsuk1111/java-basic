93a7f5eb99bf375ed37993dc7c2381bf2ddef5b0
hyunsuk1111 <epsxkf@gmail.com>
2025-02-03 19:32:57 +0900

    다형성 예제2

--- src/extends1/poly/car0/Driver.java
+++ src/extends1/poly/car0/Driver.java
@@ -4,15 +4,27 @@ public class Driver {
 
     //null 초기화
     private K3Car k3Car;
+    private Model3Car model3Car;
 
     public void setK3Car(K3Car k3Car) {
         this.k3Car = k3Car;
     }
 
+    public void setModel3Car(Model3Car model3Car) {
+        this.model3Car = model3Car;
+    }
+
     public void drive() {
         System.out.println("자동차를 운전");
-        k3Car.startEngine();
-        k3Car.pressAccelerator();
-        k3Car.offEngine();
+
+        if (k3Car != null) {
+            k3Car.startEngine();
+            k3Car.pressAccelerator();
+            k3Car.offEngine();
+        } else if (model3Car != null) {
+            model3Car.startEngine();
+            model3Car.pressAccelerator();
+            model3Car.offEngine();
+        }
     }
 }
