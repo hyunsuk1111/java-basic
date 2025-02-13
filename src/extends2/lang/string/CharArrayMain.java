b5a351e02c955faa746369b7f2f3098004950b78
hyunsuk1111 <epsxkf@gmail.com>
2025-02-13 17:09:58 +0900

    String

---
+++ src/extends2/lang/string/CharArrayMain.java
@@ -0,0 +1,13 @@
+package extends2.lang.string;
+
+public class CharArrayMain {
+
+    public static void main(String[] args) {
+        char[] charArr = new char[]{'h', 'e', 'l', 'l', 'o'};
+
+        System.out.println(charArr);
+
+        String str = "hello";
+        System.out.println("str = " + str);
+    }
+}
