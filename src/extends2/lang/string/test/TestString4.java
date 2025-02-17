d3677b4c9ae2192e4563924f3e27818ace6a5a50
hyunsuk1111 <epsxkf@gmail.com>
2025-02-17 20:08:39 +0900

    문자열 문제 풀이

---
+++ src/extends2/lang/string/test/TestString4.java
@@ -0,0 +1,10 @@
+package extends2.lang.string.test;
+
+public class TestString4 {
+
+    public static void main(String[] args) {
+        String s = "hello.txt";
+
+        System.out.println("extName = " + s.substring(5));
+    }
+}
