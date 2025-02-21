fb4ee88688b937f22d6c6f2ff68b3ebb73e37c90
hyunsuk1111 <epsxkf@gmail.com>
2025-02-21 22:17:40 +0900

    타입 안전성 오류 예제2

---
+++ src/extends2/enumeration/ex1/StringGradeEx1_1.java
@@ -0,0 +1,22 @@
+package extends2.enumeration.ex1;
+
+
+public class StringGradeEx1_1 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+
+        int basic = discountService.discount(StringGrade.BASIC, price);
+        int gold = discountService.discount(StringGrade.GOLD, price);
+        int diamond = discountService.discount(StringGrade.DIAMOND, price);
+
+        //문자열로 입력할수 있음, 타입으로 정의하는게 좋음
+        //int vip = discountService.discount("VIP", price);
+
+        System.out.println("basic = " + basic);
+        System.out.println("gold = " + gold);
+        System.out.println("diamond = " + diamond);
+    }
+}
