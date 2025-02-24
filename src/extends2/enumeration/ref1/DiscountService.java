01ab70e2aaaaced5e7f5f756273fb651f9310205
hyunsuk1111 <epsxkf@gmail.com>
2025-02-24 17:41:59 +0900

    열거형 리펙토링

---
+++ src/extends2/enumeration/ref1/DiscountService.java
@@ -0,0 +1,8 @@
+package extends2.enumeration.ref1;
+
+public class DiscountService {
+
+    public int discount(ClassGrade classGrade, int price) {
+        return price * classGrade.getDiscountPercent() / 100;
+    }
+}
